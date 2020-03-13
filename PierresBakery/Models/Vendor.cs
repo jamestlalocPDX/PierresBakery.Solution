ea3c2e3c8cade02dcff8642e06f0b3f6ab34e2c4
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-13 16:35:32 -0700

    add constructor to the Vendor class.

--- PierresBakery/Models/Vendor.cs
+++ PierresBakery/Models/Vendor.cs
@@ -8,5 +8,13 @@ namespace PierresBakery.Models
     public string Name {get; set; }
     public int Id { get; set; }
     public List<Order> Orders {get; set;}
+
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
   }
 }
