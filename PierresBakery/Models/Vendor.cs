dec90af4b3d6ef8452d0a4c0259781a5ed42e096
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-13 16:40:51 -0700

    add ClearAll method to Vendor class.

--- PierresBakery/Models/Vendor.cs
+++ PierresBakery/Models/Vendor.cs
@@ -16,5 +16,12 @@ namespace PierresBakery.Models
       Id = _instances.Count;
       Orders = new List<Order>{};
     }
+
+    public static void ClearAll()
+    {
+    _instances.Clear();
+    }
+
+    
   }
 }
