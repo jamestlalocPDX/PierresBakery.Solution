762659d4874ff988489975b19a58fde0d951e14b
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-13 17:47:30 -0700

    add an Index route to display all Vendors.

---
+++ PierresBakery/Controllers/VendorsController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PierresBakery.Models;
+
+namespace PierresBakery.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = CVendor.GetAll();
+      return View(allVendors);
+    }
+
+  }
+}
