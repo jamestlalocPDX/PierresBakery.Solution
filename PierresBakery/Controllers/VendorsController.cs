e2fd7140d2231e7c897a60f2589f3060b7067eae
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-13 18:43:56 -0700

    add a show route that links order objects to vendor objects with a dictionary.

--- PierresBakery/Controllers/VendorsController.cs
+++ PierresBakery/Controllers/VendorsController.cs
@@ -13,10 +13,30 @@ namespace PierresBakery.Controllers
       List<Vendor> allVendors = CVendor.GetAll();
       return View(allVendors);
     }
+
     [HttpGet("/vendors/new")]
     public ActionResult New()
     {
       return View();
     }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
   }
 }
