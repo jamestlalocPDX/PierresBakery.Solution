a7172e4ec4eb2c92b15b2b556b8711ee16dafd44
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-13 19:19:42 -0700

    add a root route in HomeController, and a home page in the Home directory index file.

---
+++ PierresBakery/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresBakery.Models;
+
+namespace PierresBakery.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+  }
+}
