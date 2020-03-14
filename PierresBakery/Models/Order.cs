47e50d2406dc45a80517d5350a855188997078ee
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-13 19:14:25 -0700

    add auto-implemented properties, constructor, GetAll, and ClearAll method to the Order Class.

---
+++ PierresBakery/Models/Order.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace PierresBakery.Models
+{
+  public class Order
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string Date { get; set; }
+    private static List<Order> _instances = new List<Order> {};
+
+    public Order (string title,string description, string price, string date)
+    {
+      Title= title;
+      Description = description;
+      Price = price;
+      Date = date;
+      _instances.Add(this);
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+  }
+}
